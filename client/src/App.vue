e873997079b51e052b775d525426a2b7134e5d63
Omachonu Prosper <omachonucodes@gmail.com>
2025-09-20 20:04:27 +0100

    Setup tailwind

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,9 +1,8 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1 class="ml-5">
+    Login to BEEVS
+  </h1>
+
 </template>
