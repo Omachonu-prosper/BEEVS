137164b9ce72e4347d19c6810f61a99c7a65fbff
Omachonu Prosper <omachonucodes@gmail.com>
2025-09-20 22:41:19 +0100

    Show elections on the dashboard

---
+++ client/src/components/ElectionList.vue
@@ -0,0 +1,23 @@
+<script setup>
+const elections = ["NACOS Election 2025", "NACOS Election 2024"]
+</script>
+
+<template>
+    <div class="container mx-auto mt-15 ">
+        <h1 class="mb-5 text-lg">
+            Select or create an election
+        </h1>
+        
+        <div class="grid grid-cols-4 gap-3">
+            <div 
+            v-for="(election, index) in elections" :key="index"
+            class="height-50 bg-gray-300 rounded-lg p-8 cursor-pointer hover:bg-gray-400">
+                {{ election }}
+            </div>
+
+            <div class="height-50 bg-gray-300 rounded-lg p-8 cursor-pointer hover:bg-gray-400">
+                <div class="text-5xl text-center">+</div>
+            </div>
+        </div>
+    </div>
+</template>
--- client/src/views/DashboardView.vue
+++ client/src/views/DashboardView.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Navbar from '@/components/Navbar.vue';
+import ElectionList from '@/components/ElectionList.vue';
 </script>
 
 <template>
     <Navbar />
+    <ElectionList />
 </template>
